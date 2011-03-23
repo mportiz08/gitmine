f6b71ce8f103a34a8378b91a063c6b21bc49d620
Marcus Ortiz <mportiz08@gmail.com>
2011-03-22 18:01:37 -0700

    working with csvs now

--- lib/gitmine.rb
+++ lib/gitmine.rb
@@ -1,8 +1,21 @@
-require 'nokogiri'
+require 'ostruct'
+require 'faster_csv'
 require 'octokit'
 
-module Gitmine
-  def self.transfer(path)
-    # TODO
+module Gitmine  
+  class Transfer
+    def initialize(path)
+      @csv = File.open(path).read
+    end
+    
+    def run
+      issues = []
+      FasterCSV.parse(@csv) do |row|
+        issue = OpenStruct.new
+        issue.num = row[0]
+        
+        issues << issue
+      end
+    end
   end
 end
