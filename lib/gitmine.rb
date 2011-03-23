307424a43b1126ddebb76bb3222c8a06688a5efe
Marcus Ortiz <mportiz08@gmail.com>
2011-03-22 18:15:10 -0700

    implemented basic parsing of csvs

--- lib/gitmine.rb
+++ lib/gitmine.rb
@@ -5,17 +5,41 @@ require 'octokit'
 module Gitmine  
   class Transfer
     def initialize(path)
-      @csv = File.open(path).read
+      @issues = parse(File.open(path).read)
     end
     
     def run
+      puts @issues.first
+    end
+    
+    private
+    
+    def parse(csv)
       issues = []
-      FasterCSV.parse(@csv) do |row|
+      FasterCSV.parse(csv) do |row|
         issue = OpenStruct.new
         issue.num = row[0]
-        
+        issue.status = row[1]
+        issue.project = row[2]
+        issue.tracker = row[3]
+        issue.priority = row[4]
+        issue.subject = row[5]
+        issue.assigned_to = row[6]
+        issue.category = row[7]
+        issue.target_version = row[8]
+        issue.author = row[9]
+        issue.start_date = row[10]
+        issue.due_date = row[11]
+        issue.percent_done = row[12]
+        issue.estimated_time = row[13]
+        issue.parent_task = row[14]
+        issue.created = row[15]
+        issue.updated = row[16]
+        issue.affected_version = row[17]
+        issue.description = row[18]
         issues << issue
       end
+      issues
     end
   end
 end
